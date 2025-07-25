82862db24392b6db7b38f9204dabff4b5493b6f6
SwatiPaiGhatkar <swatipaighatkar@gmail.com>
2025-07-26 09:10:49 +1000

    Added footer and GitHub url

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import "./App.css";
 
-function App() {
-  return <div className="App">Weather App</div>;
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://github.com/SwatiPaiGhatkar?tab=repositories"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Swati Pai
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/SwatiPaiGhatkar/react-weather-application"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub.
+        </a>
+      </footer>
+    </div>
+  );
 }
-
-export default App;
