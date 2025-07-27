9533087cb5dc11a02d4306ad1b07313bedc36c0c
SwatiPaiGhatkar <swatipaighatkar@gmail.com>
2025-07-27 14:04:17 +1000

    Added basic styling in CSS

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://github.com/SwatiPaiGhatkar?tab=repositories"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Swati Pai
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/SwatiPaiGhatkar/react-weather-application"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub.
-        </a>
-      </footer>
+      <div className="container">
+        <Weather />
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://github.com/SwatiPaiGhatkar?tab=repositories"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Swati Pai
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/SwatiPaiGhatkar/react-weather-application"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub.
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
